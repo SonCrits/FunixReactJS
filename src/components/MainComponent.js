b6c9916bc7ddef671e05edf252b8175e4cd206b2
Trần Trường Sơn <95740163+SonCrits@users.noreply.github.com>
2021-12-25 15:33:32 +0700

    update MainComponent.js

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -43,7 +43,7 @@ class Main extends Component {
       return (
         <DishDetail 
           dish = {this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
-          comments = {this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))[0]} 
+          comments = {this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} 
         />
       );
     }
@@ -53,7 +53,7 @@ class Main extends Component {
         <Switch>
           <Route path= '/home' component={HomePage} />
           <Route exact path= '/menu' component={() => <Menu dishes={this.state.dishes}/>}/>
-          <Route path='menu/:dishId' component={DishWithId} />
+          <Route path='/menu/:dishId' component={DishWithId} />
           <Route exact path= '/contactus' component={Contact} />
           <Redirect to="/home"/>
         </Switch>
