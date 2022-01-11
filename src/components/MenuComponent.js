289058cb300b649eb25b917b1f864f227b261bea
Trần Trường Sơn <95740163+SonCrits@users.noreply.github.com>
2022-01-11 21:41:12 +0700

    React Redux form Validate

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,17 +1,16 @@
 import React from "react";
-import { Card, CardImg, CardImgOverlay,CardTitle, Breadcrumb , BreadcrumbItem } from 'reactstrap';
+import { Card,CardBody, CardImg, CardImgOverlay,CardTitle, Breadcrumb , BreadcrumbItem } from 'reactstrap';
 import {Link} from 'react-router-dom';
 
 function RenderMenuItem({dish , onClick}) {
   return(
-    <Card>
-      <Link to={`/menu/${dish.id}`}>
+    <Link to={`/menu/${dish.id}`}>
+      <CardBody>
+        <CardTitle>{dish.name}</CardTitle>
         <CardImg width= "100%" src= {dish.image} alt = {dish.name} />
-        <CardImgOverlay>
-          <CardTitle>{dish.name}</CardTitle>
-        </CardImgOverlay>
-      </Link>
-    </Card>
+      </CardBody>
+    </Link>
+    
   );
 }
 
